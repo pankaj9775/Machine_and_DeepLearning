7189eaf0375ae91777610a70e383eb609c20b876
pankaj9775 <pankaj.9775@gmail.com>
2017-04-24 10:38:42 +0530

    Updates

---
+++ image_clothes_classifier.py
@@ -0,0 +1,97 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
+import cv2 
+import numpy as np
+
+# Assign the dimensions of images to be trained.
+img_width, img_height = 150, 150
+
+#Data directries
+train_data_dir = './data/image_clothes_identification/train'
+test_data_dir = './data/image_clothes_identification/test'
+
+
+#training parameters
+nb_train_samples = 450
+nb_test_samples = 40
+epochs = 20
+batch_size = 10
+optimizer = 'rmsprop'
+
+
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 3)
+
+
+#Define our model
+model = Sequential()
+model.add(Conv2D(32, (3, 3),input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(128, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('softmax'))
+
+#Compile the model
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizer,
+              metrics=['accuracy'])
+
+#Defining Training Image Data Generator Parameters
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+#Defining Test Image Data Generator Parameters
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
+
+test_generator = test_datagen.flow_from_directory(
+    test_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
+
+#Start Training the model
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=nb_train_samples // batch_size,
+    epochs=epochs,
+    validation_data=test_generator,
+    validation_steps=nb_test_samples // batch_size)
+
+#save the trained model
+model.save_weights('./data/image_clothes_identification/first_try.h5')
